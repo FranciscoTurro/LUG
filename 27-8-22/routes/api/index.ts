eb3f4ce10807c33dc73a76cd006071934a4aef0d
Laureano <laumaidana56@gmail.com>
2022-10-04 20:50:48 -0300

    anda un poco

--- 27-8-22/routes/api/index.ts
+++ 27-8-22/routes/api/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import blogRoutes from "./blogs";
+import usersRoutes from "./users";
 
 const router = Router();
 // todas las rutas que lleguen a /api/blogs, ejecutaran lo que se exporto de blogRoutes
 router.use("/blogs", blogRoutes);
+router.use("/users", usersRoutes);
 
 // se pueden agregar todas las rutas que se necesiten, estaran dentro de /api
 
