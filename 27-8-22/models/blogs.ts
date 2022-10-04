f4c6882f5e89ed719cbc07c8dd05eaf379534e2d
Laureano <laumaidana56@gmail.com>
2022-10-04 20:15:02 -0300

    test

--- 27-8-22/models/blogs.ts
+++ 27-8-22/models/blogs.ts
@@ -3,15 +3,10 @@ import { Schema, model } from "mongoose";
 // declaro la estructura que va a tener mi esquema/documento/tabla.
 const blogSchema = new Schema({
   title: String, // String is shorthand for {type: String}
-  author: String,
+  author: {type:Schema.Types.ObjectId,ref:"Users"},
   body: String,
-  comments: [{ body: String, date: Date }],
-  date: { type: Date, default: Date.now },
-  hidden: Boolean,
-  meta: {
-    votes: Number,
-    favs: Number,
-  },
+  comments: [{type:Schema.Types.ObjectId,ref:"Comments" }],
+  date: { type: Date, default: Date.now }
 });
 // exporto mi modelo, el cual me permite acceder a los metodos de la bd.
 export default model("Blog", blogSchema);
---
+++ 27-8-22/models/users.ts
@@ -0,0 +1,8 @@
+import { Schema, model } from "mongoose";
+
+const usersSchema = new Schema({
+    name:String,
+    mail:String,
+});
+
+export default model("Users", usersSchema);
