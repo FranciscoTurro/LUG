a93456f896d072ea86c6dbe9201607f444618085
FranciscoTurro <franciscogtrro@outlook.com.ar>
2022-10-22 14:23:02 -0300

    funciona todo bien

--- controllers/comment.ts
+++ controllers/comment.ts
@@ -1,49 +1,46 @@
-import { Request, Response } from "express";
-import { model } from "mongoose";
-import commentModel from "../models/comment";
-import blogModel from "../models/blogs";
-import { exec } from "child_process";
+import { Request, Response } from 'express';
+import { model } from 'mongoose';
+import commentModel from '../models/comment';
+import blogModel from '../models/blogs';
 
 export const commentController = {
-  post: async (request:Request, response:Response) => {
+  post: async (request: Request, response: Response) => {
     try {
-      const body = request.body //traigo del body
-      const comment = new commentModel({...body}) //Creo una instancia del modelo //... Le dan las propiedades a otro ob
+      const body = request.body; //traigo del body
+      const comment = new commentModel({ ...body }); //Creo una instancia del modelo //... Le dan las propiedades a otro ob
       const blog = await blogModel.findById(request.params.id);
       blog?.comments.push(comment.id);
       await comment.save();
       await blog?.save();
-      
-      response.status(200).send("Todo O.K");
+
+      response.status(200).send('Todo O.K');
     } catch (error) {
       console.log(`Ha ocurrido un error`);
       console.log(error);
-      response.status(500).send("Algo no esta O.K");
-    }   
+      response.status(500).send('Algo no esta O.K');
+    }
   },
 
-  delete: async (request:Request, response:Response) => {
+  delete: async (request: Request, response: Response) => {
     try {
       await commentModel.findByIdAndDelete(request.params.id);
-      response.status(200).send("Todo O.K");
+      response.status(200).send('Todo O.K');
     } catch (error) {
-      
       console.log(`Ha ocurrido un error`);
-      response.status(500).send("Algo no esta O.K");
+      response.status(500).send('Algo no esta O.K');
     }
   },
 
-  get: async (request:Request, response:Response) => {
+  get: async (request: Request, response: Response) => {
     try {
-      const blog = await blogModel.findById(request.params.id).populate("comments");
+      const blog = await blogModel
+        .findById(request.params.id)
+        .populate('comments');
       response.status(200).send(blog?.comments);
-
     } catch (error) {
       console.log(`Ha ocurrido un error`);
       console.log(error);
-      response.status(500).send("Algo no esta O.K");
+      response.status(500).send('Algo no esta O.K');
     }
-  }
+  },
 };
-
-
--- routes/api/index.ts
+++ routes/api/index.ts
@@ -1,15 +1,13 @@
-import { Router } from "express";
-import blogRoutes from "./blogs";
-import userRoutes from "./users";
-import commentRoutes from "./comments.ts";
+import { Router } from 'express';
+import blogRoutes from './blogs';
+import userRoutes from './users';
+import commentRoutes from './comments';
 
 const router = Router();
 // todas las rutas que lleguen a /api/blogs, ejecutaran lo que se exporto de blogRoutes
-router.use("/blogs", blogRoutes);
-router.use("/users", userRoutes);
-router.use("/comments", commentRoutes);
-
+router.use('/blogs', blogRoutes);
+router.use('/users', userRoutes);
+router.use('/comments', commentRoutes);
 
 // se pueden agregar todas las rutas que se necesiten, estaran dentro de /api
-
 export default router;
